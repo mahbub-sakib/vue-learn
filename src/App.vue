bca0ca5fc657d8a5a95f2fefa60f70e81f9bdb7a
mahbub-sakib <mahbub.sakib418@gmail.com>
2025-02-22 18:26:06 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+const name = 'sakib';
+function sayName(num) {
+  return num + 'sakib'
+}
+
+const names = ref(['Sakib', 'Abony', 'Sadiq'])
+
+</script>
+
+<template>
+  <h1 class="red">Hellow world!!!</h1>
+
+  <div>{{ false ? 'hello' : 'bye' }} {{ name }}</div>
+  <div>{{ sayName(10) }}</div>
+
+  <h1 v-if="false">My name is {{ sayName(10) }}</h1>
+  <h1 v-else-if="false">My name is Abony</h1>
+  <h1 v-else>My name is Sadiq</h1>
+
+  <h1 v-for="(number, index) in names">
+    {{ number }} {{ index }}
+  </h1>
+
+</template>
+
+<style>
+.red {
+  color: red;
+}
+</style>
